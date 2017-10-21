e446233026c62c0f79c7d0bbe4a99c37548937c0
theriyadhu <32952517+theriyadhu@users.noreply.github.com>
2017-10-21 14:31:46 +0530

    Add files via upload

---
+++ python_players/conditional sts.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse032
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse032 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+        age=int(input("please enter a integer"))
+        if age==20:
+            print("eligible")
+        else:
+            print("not eligible")
+
+
+if __name__ == '__main__':
+    main()
