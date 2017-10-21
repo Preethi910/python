730dc791dd5fb801c25a05b0ca78164fe012cb96
theriyadhu <32952517+theriyadhu@users.noreply.github.com>
2017-10-21 13:51:32 +0530

    Add files via upload

---
+++ python_players/str opr.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse032
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse032 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    string="programmer"
+    print(string.count('r'))
+    print(string.find('g'))
+    print(string.replace('m','y'))
+
+if __name__ == '__main__':
+    main()
