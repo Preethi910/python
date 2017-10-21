9598ed94cb2471372d655c86f07ba7629884308b
theriyadhu <32952517+theriyadhu@users.noreply.github.com>
2017-10-21 11:54:23 +0530

    Add files via upload

---
+++ python_players/add & sub & division.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      16cse032
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse032 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+  a=1
+  b=2
+  c=3
+  d=4
+  e=5
+  f=6
+  g=7
+  h=8
+  i=9
+  j=10
+  print(a+b+c+d+e+f+g+h+i+j)
+  print(a-b-c-d-e-f-g-h-i-j)
+  print(a/b/c/d/e/f/g/h/i/j)
+if __name__ == '__main__':
+    main()
