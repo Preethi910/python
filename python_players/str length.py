ba1df9b84f9b62b9b2c534c079b9a832733855d0
theriyadhu <32952517+theriyadhu@users.noreply.github.com>
2017-10-21 13:35:12 +0530

    Add files via upload

---
+++ python_players/str length.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse032
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse032 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    string="programmer"
+    print len(string)
+
+if __name__ == '__main__':
+    main()
