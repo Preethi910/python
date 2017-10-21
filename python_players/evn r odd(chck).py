4043ac9a430cd4a1fbb20f2302457e8564b3a2af
theriyadhu <32952517+theriyadhu@users.noreply.github.com>
2017-10-21 16:12:48 +0530

    Add files via upload

---
+++ python_players/evn r odd(chck).py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse032
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse032 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main(xx):
+    a=xx
+    if(a%2==0):
+        print("the given num is even")
+    else:
+         print("the given num is odd")
+
+
+
+
+
+if __name__ == '__main__':
+    main(28)
